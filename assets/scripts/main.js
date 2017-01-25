a4474f67ba498fe9bdcad4ae6f5686aa7b4c7394
Ross Young <ryoung@orthosynetics.com>
2017-01-25 17:45:16 -0600

    adds animated horn and color grid

--- assets/scripts/main.js
+++ assets/scripts/main.js
@@ -83,8 +83,8 @@
         strokeWidth: 3,
         fillOpacity: .6,
         mask: ellipse
-         
       });
+
       ellipse.attr({
         fill: '#fff',
         opacity: .3
@@ -96,36 +96,118 @@
         strokeWidth: 5
       });
 
+      
       function blink(){
         ellipse.animate({ry:1}, 220, function(){
           ellipse.animate({ry: 90}, 300);
         });
       };
+
       // Recall blink method once every 3 seconds
       setInterval(blink, 1000);
 
-      // circle1.attr({
-      //   fill: 'coral',
-      //   stroke: 'coral',
-      //   strokeOpacity: .3,
-      //   strokeWidth: 10
-      // });
-      // circle2.attr({
-      //   fill: 'coral',
-      //   stroke: 'coral',
-      //   strokeOpacity: .3,
-      //   strokeWidth: 10
-      // });
-      
-      
-      // square.attr({
-      //   fill: 'lightblue',
-      //   stroke: 'lightblue',
-      //   strokeOpacity: .3,
-      //   strokeWidth: 10
-      // });
 
+
+      // Bullhorn
+      var bullhorn = Snap('#bullhorn'),
+        soundWave1 = bullhorn.select('path#soundwave1'), 
+        soundWave2 = bullhorn.select('path#soundwave2'),
+        horn = bullhorn.select('g.horn');
       
+      soundWave1.attr({
+        "fill-opacity": 1
+      });
+
+      soundWave2.attr({
+        "fill-opacity": 1
+      });
+
+      function hornSound(){
+        horn.animate({transform:'t-2 0'}, 1000, mina.elastic);
+        soundWave1.animate({transform:'t20 -5', "fill-opacity": 0}, 800, mina.ease);
+        soundWave2.animate({transform:'t18 -5', "fill-opacity": 0}, 800, mina.easeout, function(){
+          horn.animate({transform:'t0 0'}, 500, mina.ease);
+          soundWave1.animate({transform:'t0 0', "fill-opacity": 1}, 1200, mina.ease);
+          soundWave2.animate({transform:'t0 0', "fill-opacity": 1}, 1000, mina.easein);
+        });
+      };
+      
+      horn.click(hornSound);
+
+
+      // Color Grid
+      var colors = ['#00eb76', '#87cefa', '#ffe34d', '#f08080'];
+      var color = colors[Math.floor(Math.random()*colors.length)];
+      // console.log(color);
+
+      var width = 50;
+      var height = 50;
+      var cols = 10;
+      var rows = 10;
+
+      function gridMaker() {
+          var svgGrid = Snap('#squaremultiply');
+          var squareEl = svgGrid.rect(0, 0, 50, 50);
+          squareEl.attr({
+            fill: color
+          });
+          for ( i=0; i<cols; i++) {
+            for ( j=0; j<rows; j++) {
+              if (i || j ) {   // skip the first cell
+                  var idName = "square"+(i*j);
+                  var h = svgGrid.rect(0, 0, 50, 50);
+                  var randomizer = Math.floor(Math.random()*4 + 1);
+                  // console.log(randomizer);
+                  var color1 = '#00eb76';
+                  var color2 = '#87cefa';
+                  var color3 = '#ffe34d';
+                  var color4 = '#f08080';
+                  var xRandomizer = Math.floor(Math.random()*50 + 1);
+                  xRandomizer *= Math.floor(Math.random()*2) == 1 ? 1 : -1;
+                  var yRandomizer = Math.floor(Math.random()*30) + 1;
+                  yRandomizer *= Math.floor(Math.random()*2) == 1 ? 1 : -1;
+                  
+                  switch (randomizer) {
+                    case 1:
+                      h.attr({
+                        fill: color1,
+                        id: idName
+                      });
+                      h.animate({transform:'t-2 0'}, 1000, mina.elastic);
+                      break;
+                    
+                    case 2:
+                      h.attr({
+                        fill: color2,
+                        id: idName
+                      });
+                      break;
+
+                    case 3:
+                      h.attr({
+                        fill: color3,
+                        id: idName
+                      });
+                      break;
+                    case 4:
+                      h.attr({
+                        fill: color4,
+                        id: idName
+                      });
+                      break;
+                  }
+                    
+                  var x =i*width;
+                  var y = j*height;
+                  var setSquare = "translate("+x+","+y+")";
+                  h.transform(setSquare);
+              }
+            }
+        };
+      };
+      gridMaker();
+      // setInterval(gridMaker, 5000);
+
 
     }
   };
